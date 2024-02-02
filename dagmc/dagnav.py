2c28814ecfd420009b058002160d1403a0f584b5
Patrick Shriwise <pshriwise@gmail.com>
2024-02-02 12:40:09 -0600

    Including model in equality and hash methods for DAGSet

--- dagmc/dagnav.py
+++ dagmc/dagnav.py
@@ -129,11 +129,11 @@ class DAGSet:
         if geom_dimension == -1 and category is None:
             raise ValueError(f"{identifier} has no category or geom_dimension tags assigned.")
 
-    def __eq__(self, other) -> bool:
-        return self.handle == other.handle
+    def __eq__(self, other):
+        return self.model == other.model and self.handle == other.handle
 
-    def __hash__(self) -> int:
-        return hash(self.handle)
+    def __hash__(self):
+        return hash((self.handle, id(self.model)))
 
     def __repr__(self):
         return f'{type(self).__name__} {self.id}, {self.num_triangles()} triangles'
